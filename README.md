8556d7b863ec3357385bfdf4da55e6112233ce52
mahfuja5768 <mahfujaahsanmuna@gmail.com>
2023-08-31 10:38:57 +0600

    add functions

--- README.md
+++ README.md
@@ -1,15 +1,31 @@
-# phone-hunter-api
+API Links
 
-## API Links
+All News Category
 
-### Phone Search
-URL Format: https://openapi.programming-hero.com/api/phones?search=${searchText}
-
-Example: https://openapi.programming-hero.com/api/phones?search=iphone
+URL: https://openapi.programming-hero.com/api/news/categories
 
 
-### Phone detail url:
-URL Format: https://openapi.programming-hero.com/api/phone/${id}
+
+All news in a Category
+
+URL Format: https://openapi.programming-hero.com/api/news/category/{category_id}
 
 
-Example: https://openapi.programming-hero.com/api/phone/apple_iphone_13_pro_max-11089
+
+Example: https://openapi.programming-hero.com/api/news/category/01
+
+
+
+News detail URL:
+
+URL Format: https://openapi.programming-hero.com/api/news/{news_id}
+
+
+
+Example: https://openapi.programming-hero.com/api/news/0282e0e58a5c404fbd15261f11c2ab6a
+
+
+
+Missing Data:
+
+Here, the total view and author name are null
--- main.js
+++ main.js
@@ -1,107 +1,115 @@
-function loadPhones(searchText, isShowAll) {
-  fetch(`https://openapi.programming-hero.com/api/phones?search=${searchText}`)
-    .then((res) => res.json())
-    .then((data) => {
-      const phonesData = data.data;
-      displayPhones(phonesData, isShowAll);
-    });
-}
-
-const displayPhones = (phones, isShowAll) => {
-  // console.log(phones);
-  const phoneContainer = document.getElementById("phone_container");
-
-  phoneContainer.textContent = "";
-
-  const showAllContainer = document.getElementById("show_all_container");
-  if (phones.length > 12 && !isShowAll) {
-    showAllContainer.classList.remove("hidden");
-  } else {
-    showAllContainer.classList.add("hidden");
-  }
-  if (!isShowAll) {
-    phones = phones.slice(0, 6);
-  }
-  // console.log(isShowAll, "is show all");
-
-  phones.forEach((phone) => {
-    console.log(phone);
-    const phoneItem = document.createElement("div");
-    phoneItem.innerHTML = `
-        <div class="card bg-base-100 shadow-xl">
-        <figure class="mx-5 mt-5 bg-blue-100 rounded-xl">
-          <img src=${phone.image} alt="Shoes" class="rounded-xl m-10" />
-        </figure>
-        <div class="card-body items-center text-center space-y-3">
-          <h2 class="text-2xl font-bold">${phone.phone_name}</h2>
-          <p class="w-3/4 ">There are many variations of passages of available, but the majority have suffered</p>
-          <div class="card-actions">
-            <button onclick="handleShowDetails('${phone.slug}')" class="btn btn-primary my-3 rounded-lg bg-blue-600 text-white border-none hover:bg-blue-800">Show Details</button>
-          </div>
-        </div>
-      </div>
-        `;
-    phoneContainer.appendChild(phoneItem);
-  });
-  loader(false);
-};
-
-
-const handleShowDetails = async (id) => {
-  loader(true);
+const loadAllNews = async () => {
   const res = await fetch(
-    `https://openapi.programming-hero.com/api/phone/${id}`
+    "https://openapi.programming-hero.com/api/news/categories"
   );
   const data = await res.json();
-  const details = data.data;
-  phoneDetails(details);
+  //   console.log(data);
+  const allNews = data.data.news_category; /* .slice(0, 4) */
+  loadCategoriesName(allNews);
 };
 
-function phoneDetails(phone) {
-  console.log(phone);
-  phnDetails.showModal();
-  const modal = document.getElementById("modal_body");
-  modal.textContent = "";
-  const div = document.createElement("div");
-  div.innerHTML = `
-  <div class="card">
-  <figure class=" bg-blue-100 rounded-xl">
-    <img src=${phone.image} alt="Shoes" class="rounded-xl my-10" />
-  </figure>
-  <div class="card-body text-start">
-    <h2 class="text-xl font-bold ">${phone.name}</h2>
-    <p class='w-full text-sm'>It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout.
-     </p>
-     <p><span class="font-semibold text-lg">Storage: </span>${phone?.mainFeatures?.storage}</p>
-     <p><span class="font-semibold text-lg">Display Size: </span>${phone?.mainFeatures?.displaySize}</p>
-     <p><span class="font-semibold text-lg">Release Date: </span>${phone?.releaseDate}</p>
-     <p><span class="font-semibold text-lg">Slug: </span>${phone?.slug}</p>
-    </div>
-</div>
-  `;
-  modal.appendChild(div);
-  loader(false)
+function loadCategoriesName(allNews) {
+  //   console.log(allNews);
+  const news_categories = document.getElementById("news_categories");
+  allNews.forEach((category) => {
+    const div = document.createElement("div");
+    div.innerHTML = `<p onclick="handleCategoryId('${category.category_id}'); handleCategoryName('${category.category_name}')"> ${category.category_name} </p>`;
+    news_categories.appendChild(div);
+  });
 }
 
-const loader = (isLoading) => {
-  const loading = document.getElementById("loader");
-  if (isLoading) {
-    loading.classList.remove("hidden");
-  } else {
-    loading.classList.add("hidden");
+const handleCategoryName = (categoryName) => {
+  const category_name = document.getElementById("category_name");
+  category_name.innerText = categoryName;
+};
+
+const handleCategoryId = async (categoryId) => {
+  // console.log(categoryId);
+  const res = await fetch(
+    `https://openapi.programming-hero.com/api/news/category/${categoryId}`
+  );
+  const data = await res.json();
+  const categoryNewsAll = data.data;
+  displayNews(categoryNewsAll);
+};
+
+const displayNews = (categoryNewsAll) => {
+  const news_counter = document.getElementById("news_counter");
+  const news_container = document.getElementById("news_container");
+  news_container.textContent = "";
+  const totalNews = categoryNewsAll.length;
+  //   console.log(totalNews);
+  news_counter.innerText = totalNews;
+
+  if(totalNews !== 0){
+     categoryNewsAll.forEach((categoryNews) => {
+      //  console.log(categoryNews);
+       const div = document.createElement("div");
+       div.innerHTML = `
+       <div class="card flex p-4 card-side bg-base-100 shadow-xl">
+       <figure>
+         <img
+           src="${categoryNews.thumbnail_url}"
+           alt="Movie" class="flex-1"
+         />
+       </figure>
+       <div class="card-body w-1/2">
+   
+         <h4 class="card-title text-2xl font-bold">${categoryNews.title}</h4>
+         <p class="card-text">${categoryNews.details.slice(0, 400)}</p>
+   
+   <div class="flex justify-between">
+   <div class="flex">
+    <div><img src="${
+      categoryNews.author.img ? categoryNews.author?.img : "no image available"
+    }" alt="" style="width:50px; height:50px; border-radius : 50%"></div>
+     <div class="ps-2">
+      <p class="text-lg font-bold">${
+        categoryNews.author.name
+          ? categoryNews.author.name
+          : "No author name available"
+      } <br>
+       <small>${categoryNews.author.published_date}</small> </p>
+     </div>
+     </div>
+     <div class="text-lg font-bold">Viewes : ${
+       categoryNews.total_view ? categoryNews.total_view : "Not available"
+     }</div>
+     <div class="text-lg font-bold">Rating : ${
+       categoryNews.rating.number ? categoryNews.rating.number : "Not available"
+     }</div>
+     <div>
+     <button class="btn btn-outline" onclick="loadNewsDetails('${
+       categoryNews._id
+     }')">Show more</button></div>
+    </div>
+    </div>
+   
+       
+       </div>
+       `;
+       news_container.appendChild(div);
+     });
+  }
+  else{
+   news_container.innerHTML = `<h1 class="text-3xl font-semibold text-center text-red-600">Sorry! no news available</h1>`
   }
 };
 
-const searchField = document.getElementById("search_field");
-const handleSearch = (isShowAll) => {
-  loader(true);
-  const searchText = searchField.value;
-  loadPhones(searchText, isShowAll);
-};
 
-const handleShowAll = () => {
-  handleSearch(true);
-  searchField.value = "";
-};
 
-// loadPhones("12");
+const loadNewsDetails =async(news_id)=>{
+  const res = await fetch (`https://openapi.programming-hero.com/api/news/${news_id}`);
+  const data = await res.json();
+  // console.log(data.data[0]);
+  const newsDetails = data.data[0];
+  displayDetails(newsDetails);
+}
+
+const displayDetails =(newsDetails)=>{
+  
+}
+
+handleCategoryId("04");
+
+loadAllNews();
